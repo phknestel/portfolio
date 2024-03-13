6362e93c1a8e4f38ec016890adbac0eaebe97b3c
KNDL <ph.knestel@yahoo.de>
2024-03-13 13:01:51 +0900

    update navigation

--- portfolio-phkn/src/app/pages/pages.ts
+++ portfolio-phkn/src/app/pages/pages.ts
@@ -5,4 +5,5 @@ export const pages = [
   { index: 3, name: 'education.md', route: '/education' },
   { index: 4, name: 'projects.md', route: '/projects' },
   { index: 6, name: 'bachelors_thesis.md', route: '/bachelors_thesis' },
+  { index: 7, name: 'masters_thesis.md', route: '/masters_thesis'},
 ];
